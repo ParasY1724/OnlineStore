76a8022fc47e3dcb4fab1c954422bb696e95731a
ParasY1724 <parasyerunkar174@gmail.com>
2024-06-19 21:15:23 +0530

    Authentication & Validation (Login | SignUp) Implementation

--- app.js
+++ app.js
@@ -1,25 +1,50 @@
-const path = require('path');
+require('dotenv').config();
 
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const session = require('express-session');
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 const app = express();
-
+const MongoDBStore = require('connect-mongodb-session')(session);
+const csrf = require('csurf');
+const flash = require('connect-flash');
 const mongoose = require('mongoose');
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+const store = new MongoDBStore({
+  uri : MONGODB_URI,
+  collection : 'sessions'
+});
+
+
+
+const crsfProtection = csrf();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
+
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(session({secret : 'my secret', resave : false,saveUninitialized : false ,store:store}));
+
+app.use(crsfProtection);
+
+app.use(flash());
+        
 app.use((req, res, next) => {
-  User.findById("666d95ea679ca87b859baefc")
+  if(!req.session.user){
+    return next();
+  }
+  User.findById(req.session.user._id)
     .then(user => {
       req.user = user;
       next();
@@ -27,25 +52,21 @@ app.use((req, res, next) => {
     .catch(err => console.log(err));
 });
 
+app.use((req,res,next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
+})
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
+
 
 app.use(errorController.get404);
 
-mongoose.connect("mongodb+srv://parasyerunkar:AeaICWhXR5teNZd7@store.axahzoz.mongodb.net/?retryWrites=true&w=majority&appName=Store")
+mongoose.connect(MONGODB_URI)
 .then(result => {
-  User.findOne().then(user => {
-    if (!user){
-      const user = new User({
-        name:'tester001',
-        email:'abc@gmail.com',
-        cart : {
-          items:[]
-        }
-      });
-      user.save();
-    }
-  });
   app.listen(3000);
 })
 .catch(err => {
