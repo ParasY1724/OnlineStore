af1341cee913c35a459c8a6f1d3ec5a601b1f1b8
ParasY1724 <parasyerunkar174@gmail.com>
2024-07-08 00:02:44 +0530

    "Auth Login/Signup Endpoints Added"

---
+++ app.js
@@ -0,0 +1,82 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const multer = require('multer');
+
+const feedRoutes = require('./routes/feed');
+const authRoutes = require('./routes/auth');
+
+const app = express();
+
+
+
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, 'images');
+    },
+    filename: function(req, file, cb) {
+      cb(null, file.originalname);
+    }
+});
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === 'image/png' ||
+    file.mimetype === 'image/jpg' ||
+    file.mimetype === 'image/jpeg'
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
+
+// app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
+app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use(bodyParser.json()); // application/json
+app.use(
+  multer({ storage: storage, fileFilter: fileFilter }).single('image')
+);
+
+
+app.use('/feed', feedRoutes);
+app.use('/auth', authRoutes);
+
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  const data = error.data;
+  res.status(status).json({ message: message, data: data });
+});
+
+mongoose
+  .connect(
+    ,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      dbName: 'BlogApp',
+    }
+  )
+  .then(result => {
+    const server = app.listen(8080);
+    const io = require('./socket').init(server);
+    io.on('connection', socket => {
+      console.log('Client connected');
+    });
+  })
+  .catch(err => console.log(err));
